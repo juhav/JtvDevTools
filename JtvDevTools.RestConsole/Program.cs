c7609fe8aea3996071f242d18fd06513c9a92d48
juhav <juhav@users.noreply.github.com>
2023-02-27 15:19:15 +0200

    Update Program.cs

--- JtvDevTools.RestConsole/Program.cs
+++ JtvDevTools.RestConsole/Program.cs
@@ -1,7 +1,15 @@
-﻿using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Diagnostics;
 using System.Text;
 
 namespace JtvDevTools.RestConsole;
+
+public class RestConsoleConfig
+{
+    public string? LibraryFolder { get; set; }    
+
+    public string? VariablesFile { get; set; }  
+}
 
 internal class Program
 {
@@ -9,18 +17,49 @@ internal class Program
     {
         try
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("API operation file is missing.");
+                return;
+            }
+
             Console.Clear();
 
             var path = AppDomain.CurrentDomain.BaseDirectory;
-            var fileName = Path.Combine(path, "data\\variables.txt");
+            var jsonConfigFile = Path.Combine(path, "JtvDevTools.RestConsole.json");
+
+            var json = File.ReadAllText(jsonConfigFile, Encoding.UTF8);
+            var config = System.Text.Json.JsonSerializer.Deserialize<RestConsoleConfig>(json);
+
+            if (config == null)
+            {
+                Console.WriteLine("JtvDevTools.RestConsole.json is invalid!");
+                return;
+            }
+
+            if (!Directory.Exists(config.LibraryFolder))
+            {
+                Console.WriteLine($"Folder not found '{config.LibraryFolder}'!");
+                return;
+            }
+
+            var fileName = Path.Combine(config.LibraryFolder, config.VariablesFile ?? "");
+
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine($"File not found '{config.LibraryFolder}'!");
+                return;
+            }
+
             var variablesText = File.ReadAllText(fileName, Encoding.UTF8);
             var variables = Utils.GetKeyValuePairs(variablesText);
+            var operationFile = Path.Combine(config.LibraryFolder, args[0]);
 
-            if (File.Exists(args[0]))
+            if (File.Exists(operationFile))
             {
                 var http = new HttpService();
                 var parser = new Models.Parser(variables);
-                var requestText = File.ReadAllText(args[0]);
+                var requestText = File.ReadAllText(operationFile);
 
                 parser.Parse(requestText);
 
